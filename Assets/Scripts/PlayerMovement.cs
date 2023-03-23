4419531e32b96b8ea24512a4d360e69c1ab6300c
Shane O'Brien <guesswho@shane.pizza>
2023-03-23 10:58:55 +0000

    Updating the player move script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,25 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         Debug.Log("You moved the player!");
+        
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("You are moving the players!");
+        
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
+        Debug.Log("You are moving the players!");
         Debug.Log("You are moving the players!");
     }
 }
